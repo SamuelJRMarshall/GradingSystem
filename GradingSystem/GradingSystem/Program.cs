bab9bc9aeb2e7ad7a47d2c9aab6be9952330011e
Sam Marshall <sam.marshall014@gmail.com>
2019-07-01 22:17:42 +0100

    Initial commit

---
+++ GradingSystem/GradingSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GradingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
